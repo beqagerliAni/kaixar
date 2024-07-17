9951155879bafd39bbb46e32ec0e9235ce4381ba
beqagerliAni <157922335+beqagerliAni@users.noreply.github.com>
2024-07-17 20:12:02 +0400

    k

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -6,16 +6,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './enetit/userentit';
 import { JwtModule, JwtService } from '@nestjs/jwt';
 import { APP_GUARD } from '@nestjs/core';
-import { AuthGuard } from 'src/gurad/gurad';
 
 @Module({
     controllers: [UserConrtoller],
-    providers: [UserService,UserRepository,JwtService, 
-      {
-        provide: APP_GUARD,
-        useClass: AuthGuard,
-      },
-    ],
+    providers: [UserService,UserRepository,JwtService],
     imports:[TypeOrmModule.forFeature([UserEntity]),
     JwtModule.register({
       global: true,
